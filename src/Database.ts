9a4cadbf552e963394a6f84530b88e6f37ea265d
thetayloredman <nutdriver716@gmail.com>
2021-02-15 19:15:08 -0800

    add base [v2]

---
+++ src/Database.ts
@@ -0,0 +1,161 @@
+/* eslint-disable no-use-before-define */
+/**
+ * semsql
+ * Copyright (C) 2020  BadBoyHaloCat
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import sqlite from 'better-sqlite3';
+
+type DBColumnOptions = [string, ...string[]];
+
+interface CREATEStatement {
+    TABLE: (name: string) => CREATE_TABLECaller;
+}
+type CREATE_TABLECaller = (...columns: DBColumnOptions[]) => void;
+
+interface INSERTStatement {
+    INTO: (name: string) => { VALUES: INSERT_INTOCaller };
+    // TODO: add INSERT OR REPLACE INTO
+}
+type INSERT_INTOCaller = (params: any[]) => void;
+
+type SELECT_FROMStatement = {
+    FROM: (table: string, whereCall?: { WHERE: [string, '=' | '!=' | '>' | '>=' | '<' | '<=', any][] }) => { [column: string]: any };
+};
+
+export default class Database {
+    public constructor(dbFile = './data/semsql.db') {
+        this.file = dbFile;
+        this.db = new sqlite(dbFile);
+
+        // init props of non-callables
+        this.CREATE = {
+            TABLE: (name: string) => {
+                return (...columns: DBColumnOptions[]) => {
+                    // parse columns
+                    let initStr = `CREATE TABLE "${this._sanitize(name)}" (`;
+
+                    for (let i = 0; i < columns.length; i++) {
+                        const column = columns[i];
+                        initStr += `"${this._sanitize(column[0])}" "${this._sanitize(column[1])}"`;
+                        for (const option of column.slice(2)) {
+                            initStr += ` "${this._sanitize(option)}"`;
+                        }
+                        if (i !== columns.length - 1) {
+                            // avoid trailing commas
+                            initStr += ',';
+                        }
+                    }
+
+                    initStr += ');';
+                    this.db.exec(initStr);
+                };
+            }
+        };
+        this.INSERT = {
+            // TODO: INSERT OR REPLACE INTO
+            INTO: (name: string) => {
+                return {
+                    // TODO: allow many values inserted at once
+                    VALUES: (params: any[]) => {
+                        let queryStr = `INSERT INTO "${this._sanitize(name)}" VALUES (`;
+                        const queryParams = [];
+
+                        for (let i = 0; i < params.length; i++) {
+                            const param = params[i];
+
+                            queryStr += '?';
+                            queryParams.push(param);
+
+                            if (i !== params.length - 1) {
+                                queryStr += ',';
+                            }
+                        }
+
+                        queryStr += ');';
+
+                        this.db.prepare(queryStr).run(queryParams);
+                    }
+                };
+            }
+        };
+    }
+
+    public file: string;
+    public db: sqlite.Database;
+
+    // non-callables
+    public CREATE: CREATEStatement;
+    public INSERT: INSERTStatement;
+
+    // callables
+    public SELECT(...columns: string[]): SELECT_FROMStatement {
+        columns = columns.map((c) => this._sanitize(c));
+
+        return {
+            // TODO: inner joins, WHERE clauses, etc
+            FROM: (table: string, whereCall?: { WHERE: [string, '=' | '!=' | '>' | '>=' | '<' | '<=', any][] }) => {
+                if (columns.length === 1 && columns[0] === '*') {
+                    if (!whereCall?.WHERE || whereCall?.WHERE?.length === 0) {
+                        return this.db.prepare(`SELECT * FROM "${this._sanitize(table)}"`).all();
+                    } else {
+                        for (const c of whereCall.WHERE) {
+                            // ensure only authorized operators
+                            if (!['=', '!=', '>', '>=', '<', '<='].includes(c[1])) {
+                                throw new Error('Invalid SQL match operator.');
+                            }
+                        }
+
+                        let whereTxt = `WHERE "${this._sanitize(whereCall.WHERE[0][0])}" ${whereCall.WHERE[0][1]} ?`;
+                        const queryParams = [whereCall.WHERE[0][2]];
+                        for (const call of whereCall.WHERE) {
+                            whereTxt += ` AND "${this._sanitize(call[0])}" ${call[1]} ?`;
+                            queryParams.push(call[2]);
+                        }
+
+                        return this.db.prepare(`SELECT * FROM "${this._sanitize(table)}" ${whereTxt}`).all(queryParams);
+                    }
+                } else if (columns.includes('*')) {
+                    throw new Error('Special column "*" cannot be used in a union call. Use "*" on it\'s own.');
+                } else {
+                    if (!whereCall?.WHERE || whereCall?.WHERE?.length === 0) {
+                        return this.db.prepare(`SELECT "${columns.join('","')}" FROM "${this._sanitize(table)}"`).all();
+                    } else {
+                        for (const c of whereCall.WHERE) {
+                            // ensure only authorized operators
+                            if (!['=', '!=', '>', '>=', '<', '<='].includes(c[1])) {
+                                throw new Error('Invalid SQL match operator.');
+                            }
+                        }
+
+                        let whereTxt = `WHERE "${whereCall.WHERE[0][0].replace(/"/g, '\\"')}" ${whereCall.WHERE[0][1]} ?`;
+                        const queryParams = [whereCall.WHERE[0][2]];
+                        for (const call of whereCall.WHERE) {
+                            whereTxt += ` AND "${this._sanitize(call[0])}" ${call[1]} ?`;
+                            queryParams.push(call[2]);
+                        }
+
+                        return this.db.prepare(`SELECT "${columns.join('","')}" FROM "${this._sanitize(table)}" ${whereTxt}`).all(queryParams);
+                    }
+                }
+            }
+        };
+    }
+
+    private _sanitize(sql: string): string {
+        return sql.replace(/"/g, '\\"');
+    }
+}
---
+++ tests/index.test.js
@@ -0,0 +1,204 @@
+/**
+ * semsql
+ * Copyright (C) 2020  BadBoyHaloCat
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const semsql = require('../dist/index').default;
+const fs = require('fs');
+
+afterAll(() => {
+    try {
+        fs.rmSync('./tests/tests-db.db');
+    } catch (e) {}
+});
+
+const db = new semsql('./tests/tests-db.db');
+
+describe('CREATE', () => {
+    describe('TABLE', () => {
+        describe('single column', () => {
+            describe('no extra attributes', () => {
+                it('TEXT', () => {
+                    db.CREATE.TABLE('SINGLE_NOATTR_TEXT')(['text', 'TEXT']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('SINGLE_NOATTR_INT')(['int', 'INT']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('SINGLE_NOATTR_CHAR')(['char', 'CHAR']);
+                });
+            });
+            describe('with PRIMARY KEY attribute', () => {
+                it('TEXT(10)', () => {
+                    db.CREATE.TABLE('SINGLE_PRIMARY_TEXT10')(['text', 'TEXT(10)', 'PRIMARY KEY']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('SINGLE_PRIMARY_INT')(['int', 'INT', 'PRIMARY KEY']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('SINGLE_PRIMARY_CHAR')(['char', 'CHAR', 'PRIMARY KEY']);
+                });
+            });
+            describe('with NOT NULL attribute', () => {
+                it('TEXT(10)', () => {
+                    db.CREATE.TABLE('SINGLE_NOTNULL_TEXT')(['text', 'TEXT', 'NOT NULL']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('SINGLE_NOTNULL_INT')(['int', 'INT', 'NOT NULL']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('SINGLE_NOTNULL_CHAR')(['char', 'CHAR', 'NOT NULL']);
+                });
+            });
+        });
+        describe('two columns', () => {
+            describe('no extra attributes', () => {
+                it('TEXT', () => {
+                    db.CREATE.TABLE('DOUBLE_NOATTR_TEXT')(['text1', 'TEXT'], ['text2', 'TEXT']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('DOUBLE_NOATTR_INT')(['int1', 'INT'], ['int2', 'INT']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('DOUBLE_NOATTR_CHAR')(['char1', 'CHAR'], ['char2', 'CHAR']);
+                });
+            });
+            describe('with one PRIMARY KEY attribute', () => {
+                it('TEXT(10)', () => {
+                    db.CREATE.TABLE('DOUBLE_PRIMARY_TEXT10')(['text1', 'TEXT(10)', 'PRIMARY KEY'], ['text2', 'TEXT']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('DOUBLE_PRIMARY_INT')(['int1', 'INT', 'PRIMARY KEY'], ['int2', 'INT']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('DOUBLE_PRIMARY_CHAR')(['char1', 'CHAR', 'PRIMARY KEY'], ['char2', 'CHAR']);
+                });
+            });
+            describe('with all NOT NULL attributes', () => {
+                it('TEXT', () => {
+                    db.CREATE.TABLE('DOUBLE_NOTNULL_TEXT')(['text1', 'TEXT', 'NOT NULL'], ['text2', 'TEXT', 'NOT NULL']);
+                });
+                it('INT', () => {
+                    db.CREATE.TABLE('DOUBLE_NOTNULL_INT')(['int1', 'INT', 'NOT NULL'], ['int2', 'INT', 'NOT NULL']);
+                });
+                it('CHAR', () => {
+                    db.CREATE.TABLE('DOUBLE_NOTNULL_CHAR')(['char1', 'CHAR', 'NOT NULL'], ['char2', 'CHAR', 'NOT NULL']);
+                });
+            });
+        });
+    });
+});
+
+describe('INSERT', () => {
+    describe('INTO', () => {
+        describe('single column', () => {
+            describe('no extra attributes', () => {
+                it('TEXT', () => {
+                    db.INSERT.INTO('SINGLE_NOATTR_TEXT').VALUES(['Hello, World!']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('SINGLE_NOATTR_INT').VALUES([13]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('SINGLE_NOATTR_CHAR').VALUES(['e']);
+                });
+            });
+            describe('with PRIMARY KEY attribute', () => {
+                it('TEXT(10)', () => {
+                    db.INSERT.INTO('SINGLE_PRIMARY_TEXT10').VALUES(['helloworld']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('SINGLE_PRIMARY_INT').VALUES([13]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('SINGLE_PRIMARY_CHAR').VALUES(['e']);
+                });
+            });
+            describe('with NOT NULL attribute', () => {
+                it('TEXT(10)', () => {
+                    db.INSERT.INTO('SINGLE_NOTNULL_TEXT').VALUES(['helloworld']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('SINGLE_NOTNULL_INT').VALUES([13]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('SINGLE_NOTNULL_CHAR').VALUES(['e']);
+                });
+            });
+        });
+        describe('two columns', () => {
+            describe('no extra attributes', () => {
+                it('TEXT', () => {
+                    db.INSERT.INTO('DOUBLE_NOATTR_TEXT').VALUES(['Hello, World!', 'Hello, Second Column!']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('DOUBLE_NOATTR_INT').VALUES([13, 5]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('DOUBLE_NOATTR_CHAR').VALUES(['e', 'f']);
+                });
+            });
+            describe('with PRIMARY KEY attribute', () => {
+                it('TEXT(10)', () => {
+                    db.INSERT.INTO('DOUBLE_PRIMARY_TEXT10').VALUES(['helloworld', 'bye_world!']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('DOUBLE_PRIMARY_INT').VALUES([13, 5]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('DOUBLE_PRIMARY_CHAR').VALUES(['e', 'f']);
+                });
+            });
+            describe('with NOT NULL attribute', () => {
+                it('TEXT(10)', () => {
+                    db.INSERT.INTO('DOUBLE_NOTNULL_TEXT').VALUES(['Hello, World!', 'Hello, Second Column!']);
+                });
+                it('INT', () => {
+                    db.INSERT.INTO('DOUBLE_NOTNULL_INT').VALUES([13, 5]);
+                });
+                it('CHAR', () => {
+                    db.INSERT.INTO('DOUBLE_NOTNULL_CHAR').VALUES(['e', 'f']);
+                });
+            });
+        });
+    });
+});
+
+describe('SELECT', () => {
+    describe('*', () => {
+        it('all values are returned', () => {
+            expect(db.SELECT('*').FROM('DOUBLE_NOATTR_TEXT')).toEqual([{ text1: 'Hello, World!', text2: 'Hello, Second Column!' }]);
+        });
+        describe('WHERE clause', () => {
+            it('prep', () => {
+                db.CREATE.TABLE('SELECT_ALL_WHERE')(['id', 'INT', 'PRIMARY KEY'], ['data', 'TEXT', 'NOT NULL']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([1, 'hello world 1']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([2, 'hello world 2']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([3, 'hello world 3']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([4, 'hello world 4']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([5, 'hello world 5']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([6, 'hello world 6']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([7, 'hello world 7']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([8, 'hello world 8']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([9, 'hello world 9']);
+                db.INSERT.INTO('SELECT_ALL_WHERE').VALUES([10, 'hello world 10']);
+            });
+            it('works', () => {
+                expect(db.SELECT('*').FROM('SELECT_ALL_WHERE', { WHERE: [['id', '=', 1]] })).toEqual([{ id: 1, data: 'hello world 1' }]);
+            });
+        });
+    });
+});
