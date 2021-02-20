a2abdd76ae4dae729172f1bb7512dc453996934e
thetayloredman <nutdriver716@gmail.com>
2021-02-19 18:58:21 -0800

    fix(_sanitize): use php-like escapes to prevent injections
    Switches Database#_sanitize to use escapes as used in
    PHP's mysql_real_escape_string, which should be secure if people use it, right?

--- src/Database.ts
+++ src/Database.ts
@@ -174,6 +174,33 @@ export default class Database {
     // If a security vulnerability is found, this function is usually
     // the one responsible.
     private _sanitize(sql: string): string {
-        return sql.replace(/"/g, '\\"');
+        // Escape characters, escapes the same as PHP's mysql_real_escape_string
+        // function.
+
+        // eslint-disable-next-line no-control-regex, no-useless-escape
+        return sql.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
+            switch (char) {
+                case '\0':
+                    return '\\0';
+                case '\x08':
+                    return '\\b';
+                case '\x09':
+                    return '\\t';
+                case '\x1a':
+                    return '\\z';
+                case '\n':
+                    return '\\n';
+                case '\r':
+                    return '\\r';
+                case '"':
+                case "'":
+                case '\\':
+                case '%':
+                    return '\\' + char; // prepends a backslash to backslash, percent,
+                // and double/single quotes
+                default:
+                    return char;
+            }
+        });
     }
 }
